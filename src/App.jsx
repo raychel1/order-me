49a5d9fb68442e3f89e0511a6f07c998832c0809
Racheal <nsaaocran15@gmail.com>
2024-06-14 01:20:16 +0000

    latest update1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './component/Header'
 
 
+
 function App(){
   return(
     <>
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,6 +7,7 @@ function Header(){
     return(
         <>
           <nav>
+          <img  className="order-logo" src={`${process.env.PUBLIC_URL}/LOGO-ORDER-ME.png`} alt="Logo" />
         <ul id="menu-links">
             <li>Home</li>
             <li>Menu</li>
@@ -17,9 +18,20 @@ function Header(){
             Sign Up/Login 
         </button>
         </ul>
-
+    <header className="overlay-container">
+        <div className='overlay-conatiner-1'>
+        <img src={`${process.env.PUBLIC_URL}/public/customer-service-logo.webp`} alt="Customer Service" className='image'/>
+        <div className="overlay">Customer Service</div>
+        <img src={`${process.env.PUBLIC_URL}/public/profile-logo.avif`} alt="Profile"  className='image'/>
+        <div className="overlay">Profile</div>
+        </div>
+    </header>
         </nav>
-
+        <div className='head-1'>
+            <div className='head-2'>
+                <h1>about to start</h1>
+            </div>
+        </div>
         </>
     );
 }
